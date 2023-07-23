c6d5abc0c2a1e07a6f5bbaeea951a6bfd16ccf6f
Sergey.Krulikovskiy <sergey.krulikovskiy@infopulse.com>
2023-07-23 22:39:52 +0300

    Delegate

---
+++ HW2/ConsoleAppDelegate/ConsoleAppDelegate/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleAppDelegate
+{
+    public delegate void PrintString(string msg);
+
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            PrintString printString = PrintInGreen;
+            printString += PrintInOrange;
+            printString("Some text");
+        }
+
+        public static void PrintInGreen(string msg)
+        {
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.WriteLine(msg);
+        }
+
+        public static void PrintInOrange(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(msg);
+        }
+    }
+}
